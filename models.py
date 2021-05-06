155d08727bbec9d055fdee3b8d13d7a8776807da
NewdleCat <mrfluffy900>
2021-05-05 21:20:55 -0700

    Update models.py

--- models.py
+++ models.py
@@ -20,4 +20,20 @@ def get_time():
 #
 ## always commit your models to avoid problems later
 
+db.define_table(
+    'drawing',
+    Field('title', requires=IS_NOT_EMPTY()),
+    Field('image_data', requires=IS_NOT_EMPTY()),
+    Field('user_email', default=get_user_email),
+)
+db.contact.user_email.readable = db.contact.user_email.writable = False 
+
+# db.define_table(
+# 	'phone',
+# 	Field('contact_id', 'reference contact'),
+# 	Field('phone_number', requires=IS_NOT_EMPTY()),
+# 	Field('phone_type', requires=IS_NOT_EMPTY()),
+# )
+# db.phone.contact_id.readable = db.phone.contact_id.writable = False 
+
 db.commit()
