b870a24c9c1c7dfb825b655b9b10f8eadc8b367e
groverburger <zachary.m.booth@gmail.com>
2021-05-06 01:34:47 -0700

    canvas resizing and brush sizes/colors

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,6 +1,5 @@
 const width = 128
 const height = 64
-const scale = 8
 
 // because the canvas needs to be big, we keep the internal
 // pixel data seperate from the canvas's actual pixel data
@@ -13,11 +12,20 @@ const setPixel = (x, y, value) => {
     x = Math.floor(x)
     y = Math.floor(y)
 
-    data[x][y] = value
+    if (x >= 0 && x < width && y >= 0 && y < height)
+        data[x][y] = value
 }
 
 const getPixel = (x, y) => {
     return data[Math.floor(x)][Math.floor(y)]
+}
+
+const drawCircle = (x, y, radius, value) => {
+    for (let r=0; r<radius; r++) {
+        for (let a=0; a<Math.PI*2; a+=Math.PI/8) {
+            setPixel(x + Math.cos(a)*r, y + Math.sin(a)*r, value)
+        }
+    }
 }
 
 const refreshCanvas = () => {
@@ -54,15 +62,30 @@ const dataToString = () => {
 }
 
 const canvas = document.createElement("canvas")
+let scale
+let brushSize = 1
+let brushColor = 1
 canvas.id = "editor"
 canvas.width = width*scale
 canvas.height = height*scale
 canvas.style.border = "dashed"
+canvas.style.position = "relative"
 document.getElementsByTagName("body")[0].appendChild(canvas)
 const ctx = canvas.getContext("2d")
 
 let mousedown = false
 let lastmx, lastmy
+
+// make the canvas dynamically resize to the window
+const calculateCanvasScale = () => {
+    scale = Math.min(Math.round((innerWidth-200)/width), 10)
+    canvas.style.left = innerWidth/2 - width*scale/2 + "px"
+    canvas.width = width*scale
+    canvas.height = height*scale
+    refreshCanvas()
+}
+calculateCanvasScale()
+window.addEventListener("resize", calculateCanvasScale)
 
 document.addEventListener("mouseup", (event) => {
     mousedown = false
@@ -72,7 +95,11 @@ canvas.addEventListener("mousedown", (event) => {
     mousedown = true
     const mx = event.offsetX/scale
     const my = event.offsetY/scale
-    setPixel(mx,my,1)
+    if (brushSize > 1) {
+        drawCircle(x,y,brushSize,brushColor)
+    } else {
+        setPixel(x,y,brushColor)
+    }
     refreshCanvas()
 })
 
@@ -80,10 +107,8 @@ canvas.addEventListener("mousemove", (event) => {
     const mx = event.offsetX/scale
     const my = event.offsetY/scale
 
-    if (!lastmx)
-        lastmx = mx
-    if (!lastmy)
-        lastmy = my
+    if (!lastmx) lastmx = mx
+    if (!lastmy) lastmy = my
 
     if (mousedown) {
         // draw a line between last point and this point
@@ -92,7 +117,12 @@ canvas.addEventListener("mousemove", (event) => {
         for (let i=0; i<=dist; i++) {
             let x = lastmx + Math.cos(angle)*i
             let y = lastmy + Math.sin(angle)*i
-            setPixel(x,y,1)
+
+            if (brushSize > 1) {
+                drawCircle(x,y,brushSize,brushColor)
+            } else {
+                setPixel(x,y,brushColor)
+            }
         }
 
         refreshCanvas()
