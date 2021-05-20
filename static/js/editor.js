86e6aac028ab140ecb4ad4d316680e77b992f18d
groverburger <zachary.m.booth@gmail.com>
2021-05-20 00:48:08 -0700

    cleaned up and reorganized editor.js

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,122 +1,127 @@
-// because the canvas needs to be big, we keep the internal
-// pixel data seperate from the canvas's actual pixel data
-let data = []
-const clearData = () => {
-    data = []
-    for (let i=0; i<width; i++) {
-        data[i] = []
-    }
-}
-clearData()
-
 // the brush
-let brush = {
+const brush = {
     color: 1,
     size: 1,
-}
+    x: undefined,
+    y: undefined,
 
-const setPixel = (x, y, value) => {
-    x = Math.floor(x)
-    y = Math.floor(y)
-
-    if (x >= 0 && x < width && y >= 0 && y < height)
-        data[x][y] = value
-}
-
-const getPixel = (x, y) => {
-    x = Math.floor(x)
-    y = Math.floor(y)
-
-    if (x < 0 || x >= 128 || y < 0 || y >= 64) return -1
-    return data[x][y]
-}
-
-const drawCircle = (x, y, radius, value) => {
-    setPixel(x,y,value)
-    for (let r=0; r<radius; r+=0.5) {
-        for (let a=0; a<Math.PI*2; a+=Math.PI/8) {
-            setPixel(x + Math.cos(a)*r, y + Math.sin(a)*r, value)
+    draw(x,y) {
+        editor.setPixel(x,y,this.color)
+        for (let r=0; r<this.size; r+=0.5) {
+            for (let a=0; a<Math.PI*2; a+=Math.PI/8) {
+                editor.setPixel(x + Math.cos(a)*r, y + Math.sin(a)*r, this.color)
+            }
         }
-    }
+    },
 }
 
-const refreshCanvas = () => {
-    // fill the entire canvas with white
-    ctx.fillStyle = "white"
-    ctx.fillRect(0,0, editorCanvas.width, editorCanvas.height)
+const editor = {
+    data: [],
+    scale: 1,
+    canvas: document.getElementById("editorCanvas"),
 
-    const dist = (x1,y1,x2,y2) => {
-        return Math.sqrt((x1-x2)**2 + (y1-y2)**2)
-    }
+    clear() {
+        this.data = []
+        for (let i=0; i<width; i++) this.data[i] = []
+    },
 
-    // go through and fill in the black pixels
-    ctx.fillStyle = "black"
-    for (let x=0; x<width; x++) {
+    setPixel(x, y, value) {
+        x = Math.floor(x)
+        y = Math.floor(y)
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            this.data[x][y] = value
+    },
+
+    getPixel(x, y) {
+        x = Math.floor(x)
+        y = Math.floor(y)
+        if (x < 0 || x >= 128 || y < 0 || y >= 64) return -1
+        return this.data[x][y]
+    },
+
+    serialize() {
+        let str = ""
         for (let y=0; y<height; y++) {
-            if (getPixel(x,y)) {
-                ctx.fillRect(x*scale,y*scale, scale,scale)
-            }
-
-            // draw the user's brush cursor
-            if (dist(x+0.5,y+0.5, brush.x,brush.y) <= brush.size) {
-                ctx.beginPath()
-                ctx.rect(x*scale,y*scale, scale,scale)
-                ctx.stroke()
+            for (let x=0; x<width; x+=4) {
+                let n = this.data[x][y] || 0
+                n += (this.data[x+1][y] || 0)*2
+                n += (this.data[x+2][y] || 0)*4
+                n += (this.data[x+3][y] || 0)*8
+                str += n.toString(16)
             }
         }
-    }
-}
+        return str
+    },
 
-const changeEditorMenuSelection = selection => {
-    // hackily refresh the menu by removing it and re-adding it
-    document.body.removeChild(document.getElementById("editorToolMenu"))
-    document.body.appendChild(fromTemplate("_editorToolMenu"))
-    calculateCanvasScale()
-
-    // highlight the selected tool
-    const menu = document.getElementById("editorToolMenu")
-    for (const thing of menu.children) {
-        if (thing.id == selection + "_editortool") {
-            thing.style.color = "#4488ff"
-            thing.style.borderColor = "#4488ff"
-            thing.style.borderStyle = "solid"
+    post() {
+        let link = window.location.href
+        let title = document.getElementById("editorTitleVal").value
+        if (title == "") {
+            alert("You need to title your Picto!")
+            return
         }
-    }
+        window.location.href = link.substring(0, link.length - 5) + "post/" + this.serialize() + "/" + title
+    },
 
-    if (selection == "brush")  brush.color = 1
-    if (selection == "eraser") brush.color = 0
-}
+    refresh() {
+        const ctx = this.canvas.getContext("2d")
+        const scale = this.scale
 
-// converts the canvas data to a hexidecimal string
-const dataToString = () => {
-    let str = ""
-    for (let y=0; y<height; y++) {
-        for (let x=0; x<width; x+=4) {
-            let n = data[x][y] || 0
-            n += (data[x+1][y] || 0)*2
-            n += (data[x+2][y] || 0)*4
-            n += (data[x+3][y] || 0)*8
-            str += n.toString(16)
+        // fill the entire canvas with white
+        ctx.fillStyle = "white"
+        ctx.fillRect(0,0, this.canvas.width, this.canvas.height)
+
+        const dist = (x1,y1,x2,y2) => {
+            return Math.sqrt((x1-x2)**2 + (y1-y2)**2)
         }
-    }
-    // return str
-    let link = window.location.href
-    let title = document.getElementById("editorTitleVal").value
-    
-    if (title == "")
-        title = "untitled"
 
-    window.location.href = link.substring(0, link.length - 5) + "post/" + str + "/" + title
+        //ctx.fillStyle = "lightblue"
+        //for (let i=12; i<64; i+=13)
+            //ctx.fillRect(0, i*this.scale, this.canvas.width, this.scale)
+
+        // go through and fill in the black pixels
+        ctx.fillStyle = "black"
+        for (let x=0; x<width; x++) {
+            for (let y=0; y<height; y++) {
+                if (this.getPixel(x,y)) {
+                    ctx.fillRect(x*scale,y*scale, scale,scale)
+                }
+
+                // draw the user's brush cursor
+                if (dist(x+0.5,y+0.5, brush.x,brush.y) <= brush.size) {
+                    ctx.beginPath()
+                    ctx.rect(x*scale,y*scale, scale,scale)
+                    ctx.stroke()
+                }
+            }
+        }
+    },
+
+    close() {
+        toggleEditor()
+        this.clear()
+    },
+
+    menuSelect(selection) {
+        // hackily refresh the menu by removing it and re-adding it
+        document.body.removeChild(document.getElementById("editorToolMenu"))
+        document.body.appendChild(fromTemplate("_editorToolMenu"))
+        calculateCanvasScale()
+
+        // highlight the selected tool
+        const menu = document.getElementById("editorToolMenu")
+        for (const thing of menu.children) {
+            if (thing.id == selection + "_editortool") {
+                thing.style.color = "#4488ff"
+                thing.style.borderColor = "#4488ff"
+                thing.style.borderStyle = "solid"
+            }
+        }
+
+        if (selection == "brush")  brush.color = 1
+        if (selection == "eraser") brush.color = 0
+    },
 }
-
-const editorCanvas = document.getElementById("editorCanvas")
-let scale
-editorCanvas.width = width*scale
-editorCanvas.height = height*scale
-const ctx = editorCanvas.getContext("2d")
-
-let mousedown = false
-let lastmx, lastmy
 
 // put things on the page
 const toggleEditor = () => {
@@ -130,7 +135,7 @@ const toggleEditor = () => {
 
     if (showEditor) {
         document.getElementById("editorDiv").appendChild(fromTemplate("_dim"))
-        document.getElementById("editorDiv").appendChild(editorCanvas)
+        document.getElementById("editorDiv").appendChild(editor.canvas)
         document.body.appendChild(fromTemplate("_editorPostButton"))
         document.body.appendChild(fromTemplate("_editorCloseButton"))
         document.body.appendChild(fromTemplate("_editorTitle"))
@@ -150,7 +155,7 @@ const toggleEditor = () => {
         // remove the editor components that are in the editorDiv
         let dim = document.getElementById("dim")
         if (dim) document.getElementById("editorDiv").removeChild(dim)
-        document.getElementById("editorDiv").removeChild(editorCanvas)
+        document.getElementById("editorDiv").removeChild(editor.canvas)
 
         document.body.appendChild(fromTemplate("_editButton"))
         document.body.appendChild(fromTemplate("_friendInput"))
@@ -169,32 +174,32 @@ const toggleEditor = () => {
 
 // make the canvas dynamically resize to the window
 const calculateCanvasScale = () => {
-    scale = Math.min(Math.round((innerWidth-200)/width), 10)
-    editorCanvas.style.left = innerWidth/2 - width*scale/2 + "px"
-    editorCanvas.style.top = innerHeight/2 - height*scale/2 + "px"
-    editorCanvas.width = width*scale
-    editorCanvas.height = height*scale
-    refreshCanvas()
+    editor.scale = Math.min(Math.round((innerWidth-200)/width), 10)
+    editor.canvas.style.left = innerWidth/2 - width*editor.scale/2 + "px"
+    editor.canvas.style.top = innerHeight/2 - height*editor.scale/2 + "px"
+    editor.canvas.width = width*editor.scale
+    editor.canvas.height = height*editor.scale
+    editor.refresh()
 
     // put the close button in the right spot
     let close = document.getElementById("editorCloseButton")
     if (close) {
-        close.style.left = innerWidth/2 + width*scale/2 - close.clientWidth/2 + "px"
-        close.style.top = innerHeight/2 - height*scale/2 - close.clientHeight/2 + "px"
+        close.style.left = innerWidth/2 + width*editor.scale/2 - close.clientWidth/2 + "px"
+        close.style.top = innerHeight/2 - height*editor.scale/2 - close.clientHeight/2 + "px"
     }
 
     let title = document.getElementById("editorTitle")
     if (title) {
-        title.style.width = editorCanvas.clientWidth/2 + "px"
+        title.style.width = editor.canvas.clientWidth/2 + "px"
         title.style.left = innerWidth/2 - title.clientWidth/2 + "px"
-        title.style.top = innerHeight/2 - height*scale/2 - title.clientHeight - 32 + "px"
+        title.style.top = innerHeight/2 - height*editor.scale/2 - title.clientHeight - 32 + "px"
     }
 
     let tools = document.getElementById("editorToolMenu")
     if (tools) {
         tools.style.top = innerHeight/2 - tools.clientHeight/2 + "px"
-        tools.style.height = editorCanvas.height
-        tools.style.left = innerWidth/2 - width*scale/2 - tools.clientWidth - 32 + "px"
+        tools.style.height = editor.canvas.height
+        tools.style.left = innerWidth/2 - width*editor.scale/2 - tools.clientWidth - 32 + "px"
         //tools.style.width = "200px"
     }
 
@@ -202,30 +207,28 @@ const calculateCanvasScale = () => {
     let post = document.getElementById("editorPostButton")
     if (post) {
         post.style.left = innerWidth/2 - post.clientWidth/2 + "px"
-        post.style.top = innerHeight/2 + editorCanvas.height/2 + 16 + "px"
+        post.style.top = innerHeight/2 + editor.canvas.height/2 + 16 + "px"
     }
 }
-calculateCanvasScale()
-window.addEventListener("resize", calculateCanvasScale)
 
 document.addEventListener("mouseup", (event) => {
     mousedown = false
 })
 
-editorCanvas.addEventListener("mousedown", (event) => {
+editor.canvas.addEventListener("mousedown", (event) => {
     mousedown = true
-    const mx = event.offsetX/scale
-    const my = event.offsetY/scale
+    const mx = event.offsetX/editor.scale
+    const my = event.offsetY/editor.scale
     brush.x = mx
     brush.y = my
 
-    drawCircle(mx,my,brush.size,brush.color)
-    refreshCanvas()
+    brush.draw(mx,my,brush.size,brush.color)
+    editor.refresh()
 })
 
-editorCanvas.addEventListener("mousemove", (event) => {
-    const mx = event.offsetX/scale
-    const my = event.offsetY/scale
+editor.canvas.addEventListener("mousemove", (event) => {
+    const mx = event.offsetX/editor.scale
+    const my = event.offsetY/editor.scale
     brush.x = mx
     brush.y = my
 
@@ -239,13 +242,19 @@ editorCanvas.addEventListener("mousemove", (event) => {
         for (let i=0; i<=dist; i++) {
             let x = lastmx + Math.cos(angle)*i
             let y = lastmy + Math.sin(angle)*i
-
-            drawCircle(x,y,brush.size,brush.color)
+            brush.draw(x,y,brush.size,brush.color)
         }
     }
 
-    refreshCanvas()
+    editor.refresh()
 
     lastmx = mx
     lastmy = my
 })
+
+// on startup
+editor.clear()
+let mousedown = false
+let lastmx, lastmy
+calculateCanvasScale()
+window.addEventListener("resize", calculateCanvasScale)
