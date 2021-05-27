f947471a04cdfc46783e2cf3aee2e1e7d59ab82b
NewdleCat <mrfluffy900>
2021-05-26 20:59:29 -0700

    Moved js stuff to index.js

---
+++ static/js/index.js
@@ -0,0 +1,26 @@
+const width = 128
+const height = 64
+
+// automatically move over to /index if user is not explicitly on /index
+let link = window.location.href
+if (link.substring(link.length-5,link.length) != "index") {
+	//window.location.href += "/index"
+}
+
+const fromTemplate = (template) => {
+	return document.getElementById(template).content.firstElementChild.cloneNode(true)
+}
+
+const addFriend = () => {
+	let link = window.location.href
+	let code = document.getElementById("codeInput").value
+	if (code == "") {
+		return
+	}
+	window.location.href = link.substring(0, link.length - 5) + "add_friend/" + code
+}
+
+const deleteImage = (id) => {
+	let link = window.location.href
+	window.location.href = link.substring(0, link.length - 5) + "delete_image/" + id
+}
