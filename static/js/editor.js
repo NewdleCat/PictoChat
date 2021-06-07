597b2f1a9634053ec23fcd0607c33be517c00fa2
groverburger <zachary.m.booth@gmail.com>
2021-06-07 14:25:51 -0700

    vuejs

--- static/js/editor.js
+++ static/js/editor.js
@@ -282,9 +282,20 @@ const editor = {
     },
 }
 
+const vuedata = {
+    showCanvas: true,
+}
+
+const vueapp = new Vue({
+    el: ".vue-target",
+    data: vuedata,
+    methods: {},
+})
+
 // put things on the page
 const toggleEditor = () => {
     editor.show = !editor.show
+    vuedata.showCanvas = editor.show
 
     const remove = e => {
         if (e) {
@@ -293,7 +304,6 @@ const toggleEditor = () => {
     }
 
     if (editor.show) {
-        document.getElementById("editorDiv").appendChild(fromTemplate("_dim"))
         document.getElementById("editorDiv").appendChild(editor.canvas)
         document.body.appendChild(fromTemplate("_editorPostButton"))
         document.body.appendChild(fromTemplate("_editorCloseButton"))
@@ -305,6 +315,7 @@ const toggleEditor = () => {
         brush.menuSelect("brush")
 
         calculateCanvasScale()
+        console.log(editor.canvas.clientWidth)
     } else {
         // remove the editor components
         remove(document.getElementById("editorPostButton"))
@@ -313,8 +324,8 @@ const toggleEditor = () => {
         remove(document.getElementById("editorToolMenu"))
 
         // remove the editor components that are in the editorDiv
-        let dim = document.getElementById("dim")
-        if (dim) document.getElementById("editorDiv").removeChild(dim)
+        //let dim = document.getElementById("dim")
+        //if (dim) document.getElementById("editorDiv").removeChild(dim)
         document.getElementById("editorDiv").removeChild(editor.canvas)
 
         if (loggedIn) document.body.appendChild(fromTemplate("_editButton"))
@@ -374,77 +385,6 @@ const calculateCanvasScale = () => {
         post.style.left = innerWidth/2 - post.clientWidth/2 + "px"
         post.style.top = innerHeight/2 + editor.canvas.height/2 + 16 + "px"
     }
-}
-
-// Apply LZW-compression to a string and return base64 compressed string.
-function zip (s) {
-    try {
-        var dict = {}
-        var data = (s + '').split('')
-        var out = []
-        var currChar
-        var phrase = data[0]
-        var code = 256
-        for (var i = 1; i < data.length; i++) {
-            currChar = data[i]
-            if (dict[phrase + currChar] != null) {
-                phrase += currChar
-            } else {
-                out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0))
-                dict[phrase + currChar] = code
-                code++
-                phrase = currChar
-            }
-        }
-        out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0))
-        for (var j = 0; j < out.length; j++) {
-            out[j] = String.fromCharCode(out[j])
-        }
-        return utoa(out.join(''))
-    } catch (e) {
-        console.log('Failed to zip string return empty string', e)
-        return ''
-    }
-}
-
-// Decompress an LZW-encoded base64 string
-function unzip (base64ZippedString) {
-    try {
-        var s = atou(base64ZippedString)
-        var dict = {}
-        var data = (s + '').split('')
-        var currChar = data[0]
-        var oldPhrase = currChar
-        var out = [currChar]
-        var code = 256
-        var phrase
-        for (var i = 1; i < data.length; i++) {
-            var currCode = data[i].charCodeAt(0)
-            if (currCode < 256) {
-                phrase = data[i]
-            } else {
-                phrase = dict[currCode] ? dict[currCode] : oldPhrase + currChar
-            }
-            out.push(phrase)
-            currChar = phrase.charAt(0)
-            dict[code] = oldPhrase + currChar
-            code++
-            oldPhrase = phrase
-        }
-        return out.join('')
-    } catch (e) {
-        console.log('Failed to unzip string return empty string', e)
-        return ''
-    }
-}
-
-// ucs-2 string to base64 encoded ascii
-function utoa (str) {
-  return window.btoa(unescape(encodeURIComponent(str)))
-}
-// base64 encoded ascii to ucs-2 string
-function atou (str) {
-  return decodeURIComponent(escape(window.atob(str)))
 }
 
 document.addEventListener("keydown", event => {
